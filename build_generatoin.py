4f9fabfc1ebb6e5f3056b4c6080806d02310b2c2
Ghvahid <146728309+Ghvahid@users.noreply.github.com>
2023-10-07 12:05:35 +0330

    building -sorted-generatoin
    generatoin append Fittness_Func
    
    print(sorted(generatoin))

--- build_generatoin.py
+++ build_generatoin.py
@@ -3,11 +3,6 @@ import numpy as np
 import pandas as pd
 
 
-rank = np.arange(-10, 11)
-print(rank)
-
-rank_rd = np.random.randint(-10,11)
-print(rank_rd)
 """"
 صبوری = patience
 running away from work = فرار از کار
@@ -17,6 +12,7 @@ height = بلندی قامت
 Well-dressed = خوش پوشی
 Lying = دروغگویی
 """
+
 crom_dic = {"hardworker": np.random.randint(-10,11),
             "age": np.random.randint(-10,11),
             "running away from work": np.random.randint(-10,11),
@@ -24,5 +20,23 @@ crom_dic = {"hardworker": np.random.randint(-10,11),
             "height": np.random.randint(-10,11),
             "Well-dressed": np.random.randint(-10,11),
             "Lying": np.random.randint(-10,11)}
-
 print(crom_dic)
+
+Fittness_Func = (crom_dic["hardworker"]**3) + (crom_dic["age"]) + (crom_dic["Lying"]*2) + (crom_dic["running away from work"]) \
+                + (crom_dic["patience"]) + (crom_dic["height"]) + (crom_dic["Well-dressed"]*2)
+print(Fittness_Func)
+generatoin = []
+for _ in range(100):
+    crom_dic = {"hardworker": np.random.randint(-10, 11),
+                "age": np.random.randint(-10, 11),
+                "running away from work": np.random.randint(-10, 11),
+                "patience": np.random.randint(-10, 11),
+                "height": np.random.randint(-10, 11),
+                "Well-dressed": np.random.randint(-10, 11),
+                "Lying": np.random.randint(-10, 11)}
+    Fittness_Func = (crom_dic["hardworker"] ** 3) + (crom_dic["age"]) + (crom_dic["Lying"] * 2) + (crom_dic["running away from work"]) + (crom_dic["patience"]) + (crom_dic["height"]) + (crom_dic["Well-dressed"] * 2)
+    generatoin.append(Fittness_Func)
+    print(crom_dic)
+    print(Fittness_Func)
+
+print(sorted(generatoin))
