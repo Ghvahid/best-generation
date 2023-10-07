c31fae1ae7edc7c1b1f1a0ca70df814d7dc21308
Ghvahid <146728309+Ghvahid@users.noreply.github.com>
2023-10-07 11:12:35 +0330

    set-crom-dic
    set the genom for each cromozom

--- build_generatoin.py
+++ build_generatoin.py
@@ -5,6 +5,24 @@ import pandas as pd
 
 rank = np.arange(-10, 11)
 print(rank)
-# crom =
+
 rank_rd = np.random.randint(-10,11)
 print(rank_rd)
+""""
+صبوری = patience
+running away from work = فرار از کار
+hardworker = سختکوش، پرتلاش
+age = سن
+height = بلندی قامت
+Well-dressed = خوش پوشی
+Lying = دروغگویی
+"""
+crom_dic = {"hardworker": np.random.randint(-10,11),
+            "age": np.random.randint(-10,11),
+            "running away from work": np.random.randint(-10,11),
+            "patience": np.random.randint(-10,11),
+            "height": np.random.randint(-10,11),
+            "Well-dressed": np.random.randint(-10,11),
+            "Lying": np.random.randint(-10,11)}
+
+print(crom_dic)
