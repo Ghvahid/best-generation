5ac5311c0e8e665bf6f5e11f1474d390d533656f
Ghvahid <146728309+Ghvahid@users.noreply.github.com>
2023-10-08 14:38:16 +0330

    Get ten_percent_of_Society
    Extraction 10%  from first society

--- build_generatoin.py
+++ build_generatoin.py
@@ -14,22 +14,43 @@ Lying = دروغگویی
 Society = جامعه
 """
 
-def First_Society():
-    first_Society = []
-    for i in range(1,101):
-        crom = {"hard-worker": np.random.randint(-10, 11),
-                    "age": np.random.randint(-10, 11),
-                    "running_away_from_work": np.random.randint(-10, 11),
-                    "patience": np.random.randint(-10, 11),
-                    "height": np.random.randint(-10, 11),
-                    "Well-dressed": np.random.randint(-10, 11),
-                    "Lying": np.random.randint(-10, 11)}
+# def First_Society():
+first_Society = []
+for i in range(1, 101):
+    crom = {"hard-worker": np.random.randint(-10, 11),
+                "age": np.random.randint(-10, 11),
+                "running_away_from_work": np.random.randint(-10, 11),
+                "patience": np.random.randint(-10, 11),
+                "height": np.random.randint(-10, 11),
+                "Well-dressed": np.random.randint(-10, 11),
+                "Lying": np.random.randint(-10, 11)}
+    Fittness_Func = (crom["hard-worker"] * 5) + (crom["age"]) - (crom["Lying"] * 3) - (crom["running_away_from_work"]) + (crom["patience"]*2) + (crom["height"]) + (crom["Well-dressed"] * 2)
+    crom["Fittness_Func"] = Fittness_Func
+    first_Society.append(crom)
 
-        Fittness_Func = (crom["hard-worker"] * 5) + (crom["age"]) - (crom["Lying"] * 3) - (crom["running_away_from_work"]) + (crom["patience"]*2) + (crom["height"]) + (crom["Well-dressed"] * 2)
-        crom["Fittness_Func"] = Fittness_Func
-        first_Society.append(crom)
+data = pd.DataFrame(first_Society, index=range(1, 101)).to_string()
+    # return data
+print(data)
 
-    data = pd.DataFrame(first_Society, index=range(1, 101)).to_string()
-    print(data)
+# print(First_Society())
 
-First_Society()
+
+ten_percent_of_Society = []
+for _ in range(10):
+    selected = np.random.randint(1,101)
+    new_so = first_Society[selected]
+    ten_percent_of_Society.append(new_so)
+new_data = pd.DataFrame(ten_percent_of_Society, index=range(101, 111)).to_string()
+print(new_data)
+
+
+
+
+def Generatoin():
+    def mutatoin():
+        pass
+
+    def cross_over():
+        pass
+
+    pass
