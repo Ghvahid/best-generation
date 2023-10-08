30737bafebf7ae3a9b7ff62541b3b3a23cbf65f5
Ghvahid <146728309+Ghvahid@users.noreply.github.com>
2023-10-08 12:50:29 +0330

    First_Society

--- build_generatoin.py
+++ build_generatoin.py
@@ -11,26 +11,25 @@ age = سن
 height = بلندی قامت
 Well-dressed = خوش پوشی
 Lying = دروغگویی
+Society = جامعه
 """
 
-# Fitt_count = []
-Nasl = []
-for i in range(1,101):
-    crom_dic = {"hard-worker": np.random.randint(-10, 11),
-                "age": np.random.randint(-10, 11),
-                "running_away_from_work": np.random.randint(-10, 11),
-                "patience": np.random.randint(-10, 11),
-                "height": np.random.randint(-10, 11),
-                "Well-dressed": np.random.randint(-10, 11),
-                "Lying": np.random.randint(-10, 11)}
+def First_Society():
+    first_Society = []
+    for i in range(1,101):
+        crom = {"hard-worker": np.random.randint(-10, 11),
+                    "age": np.random.randint(-10, 11),
+                    "running_away_from_work": np.random.randint(-10, 11),
+                    "patience": np.random.randint(-10, 11),
+                    "height": np.random.randint(-10, 11),
+                    "Well-dressed": np.random.randint(-10, 11),
+                    "Lying": np.random.randint(-10, 11)}
 
-    Fittness_Func = (crom_dic["hard-worker"] ** 3) + (crom_dic["age"]) + (crom_dic["Lying"] * 2) + (crom_dic["running_away_from_work"]) + (crom_dic["patience"]) + (crom_dic["height"]) + (crom_dic["Well-dressed"] * 2)
-    crom_dic["Fittness_Func"] = Fittness_Func
-    Nasl.append(crom_dic)
-    # Fitt_count.append(Fittness_Func)
+        Fittness_Func = (crom["hard-worker"] * 5) + (crom["age"]) - (crom["Lying"] * 3) - (crom["running_away_from_work"]) + (crom["patience"]*2) + (crom["height"]) + (crom["Well-dressed"] * 2)
+        crom["Fittness_Func"] = Fittness_Func
+        first_Society.append(crom)
 
-# print(Nasl)
-# print(Fitt_count)
-data = pd.DataFrame(Nasl, index=range(1, 101)).to_string()
-print(data)
+    data = pd.DataFrame(first_Society, index=range(1, 101)).to_string()
+    print(data)
 
+First_Society()
